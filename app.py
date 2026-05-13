4efc52790ac6838314b9f8f622b50932ed879877
Astik97 <astikm7007@gmail.com>
2026-05-13 17:11:08 +0530

    fixed render deployment

--- app.py
+++ app.py
@@ -321,4 +321,6 @@ def receive_alert():
         return jsonify({"status": "error"}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    # app.run(host='0.0.0.0', port=5000, debug=True)
