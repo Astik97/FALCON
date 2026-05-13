db11b81390b06b65272732961e9fd37e2cd7c76f
Astik97 <astikm7007@gmail.com>
2026-05-13 13:20:42 +0530

    retrieve deployment

--- app.py
+++ app.py
@@ -321,6 +321,4 @@ def receive_alert():
         return jsonify({"status": "error"}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-    # app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
