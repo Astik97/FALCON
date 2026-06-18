2d43cab306f5ea4658bf36f69c58af41e4a847ee
Astik97 <astikm7007@gmail.com>
2026-06-18 20:17:04 +0530

    final updates

--- train_yolo.py
+++ train_yolo.py
@@ -4,11 +4,11 @@ from ultralytics import YOLO
 def main():
 # Load a YOLOv8 Nano model configuration (can also use yolov8s.yaml, yolov8m.yaml etc.)
     model = YOLO('yolo8s.pt')
+    #model = YOLO('yolo8m.pt')
 
 # Train the model on your custom dataset
-    model.train(
-    # data="C:\\Users\\biswa\\Desktop\\dataset\\data.yaml",
-    data="A:\\weapon_detection_2\\train_detect_weapons\\data.yaml",
+    model.train (
+    data="custom_path_data.yaml",  # path to your dataset configuration file
     epochs=50,
     imgsz=640,
     device=0
